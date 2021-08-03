2aba0700736c279575b0dcb1462c08d5ece4efd5
Stefan Schön (Carmeq) <stefan.schoen@carmeq.com>
2021-08-03 10:34:37 +0200

    clean up for minimal working release

--- Verbrauchsrechner.py
+++ Verbrauchsrechner.py
@@ -1,13 +1,3 @@
-class Product(object):
-    def __init__(self, nr, name, buy, sell, typ):
-        self.nr = nr
-        self.name = name
-        self.buy = buy
-        self.sell = sell
-        self.type = typ
-
-
-
 class Verbrauchsrechner(object):
 
     def __init__(self, efficencyTableName = "ProductionEfficency.csv", priceListName = "ProductList.csv", consumptionListName = "ConsumptionValues.csv"):
@@ -16,16 +6,11 @@ class Verbrauchsrechner(object):
         self.consumptionListName = consumptionListName
         # Index aller Städte
         self.staedteListe = {}
-        # Index aller Waren
-        self.warenListe = {}
         # Warenverbrauch der Einwohner (Reich, Wohlhabend, Arm)
         self.verbrauchsListe = []
-        # Produktionseffizienz aller Waren
-        self.produktionsListe = []
-        # Detailinfos zu allen Waren
-        self.warenInfo = []
-
+        # einfache Liste aller Waren
         self.warenNamen = []
+        # berechneter Verbrauch basierend auf Einwohnern und Reisedauer
         self.gesamtVerbrauch = []
 
     def __repr__(self):
@@ -35,56 +20,28 @@ class Verbrauchsrechner(object):
 
 
     def prepareTables(self):
-
         # create city list
         with open(self.efficencyTableName) as fHandle:
             line = fHandle.readline()
             line = line.strip()
             for nr, name in enumerate(line.split(";")):
-                self.staedteListe[name] = nr
-            #print(self.staedteListe)
-        
-        # create product, warenlist and wareninfo 
-        with open(self.priceListName) as fHandle:
-            for nr, line in enumerate(fHandle):
-                if nr == 0:
-                    line = line.strip()
-                    if line.startswith("Product;Buy;Sell;Weight"):
-                        continue
-                    else:
-                        raise Exception(f"Format of {self.priceListName} file does not fit expected format: Product;Buy;Sell;Weight!")
-                line = line.strip()
-                values = line.split(";")
-                #print(values[0])
-                # nr -1 since first line is csv format!
-                self.warenListe[values[0]] = nr-1
-                #print(values)
-                #print(nr-1)
-                self.warenInfo.append(Product(nr-1, values[0], values[1], values[2], values[3]))
-                #print(self.warenInfo[nr-1].name)
-        
+                self.staedteListe[name] = nr        
         # create verbrauchsliste
         with open(self.consumptionListName) as fHandle:
             for nr, line in enumerate(fHandle):
                 line = line.strip()
                 if nr == 0:
-                    # print(line)
                     if line == "Ware;Reiche;Wohlhabende;Arme":
                         continue
                     else:
                         raise Exception(f"Format of Consumption file not as expected!")
                 currEff = line.split(";")
-                # print(currEff)
-                # skip warenname
                 self.verbrauchsListe.append([])
                 for value in currEff[1:]:
                     self.verbrauchsListe[nr-1].append(float(value))
                 self.warenNamen.append(currEff[0])
-    # print(self.verbrauchsListe)
     
     def calculateConsumption(self, reiche: int, wohlis: int , arme: int, tage: int = 7):
-        #print("calculating...")
-        #gesamtVerbrauch = []
         # Wochenverbrauch je 1k je Klasse
         for index, warenListe in enumerate(self.verbrauchsListe):
             currVerbrauch = warenListe[0] * (reiche / 1000) * (tage / 7)
@@ -94,8 +51,6 @@ class Verbrauchsrechner(object):
             currVerbrauch = warenListe[2] * (arme / 1000) * (tage / 7)
             self.gesamtVerbrauch[index] += currVerbrauch
             self.gesamtVerbrauch[index] = round(self.gesamtVerbrauch[index])
-        #print("done.")
-        #return gesamtVerbrauch
 
     def printGesamtverbrauch(self):
         print("\n Gesamtverbrauch der Stadt: \n")
