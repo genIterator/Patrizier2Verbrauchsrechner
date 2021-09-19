ecd201a5fd769454af4c345d5022f2522ca57975
genIterator <genericiterator@gmail.com>
2021-09-19 11:50:17 +0200

    fixed Umlaute error on windows, converted tables to comma format for github

--- Verbrauchsrechner.py
+++ Verbrauchsrechner.py
@@ -3,12 +3,10 @@ import os
 
 class Verbrauchsrechner(object):
 
-    def __init__(self, efficencyTableName = "ProductionEfficency.csv", priceListName = "ProductList.csv", consumptionListName = "ConsumptionValues.csv", stadtName = ""):
-        self.efficencyTableName = efficencyTableName
-        self.priceListName = priceListName
-        self.consumptionListName = consumptionListName
-        # Index aller Städte
-        self.staedteListe = {}
+    def __init__(self, efficencyTableFile = "ProductionEfficency.csv", priceListFile = "ProductList.csv", consumptionFile = "ConsumptionValues.csv", stadtName = ""):
+        self.efficencyTableFile = efficencyTableFile
+        self.priceListFile = priceListFile
+        self.consumptionFile = consumptionFile
         # Warenverbrauch der Einwohner (Reich, Wohlhabend, Arm)
         self.verbrauchsListe = []
         # einfache Liste aller Waren
@@ -19,27 +17,21 @@ class Verbrauchsrechner(object):
         self.lastWaren = ["Eisenerz", "Fisch", "Fleisch", "Getreide", "Hanf", "Holz", "Wolle", "Ziegel"]
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.efficencyTableName!r}, {self.priceListName!r}, {self.consumptionListName!r}, {self.staedteListe!r}, {self.warenNamen}, {self.stadtVerbrauch})"
+        return f"{self.__class__.__name__}({self.efficencyTableFile!r}, {self.priceListFile!r}, {self.consumptionFile!r}, {self.warenNamen}, {self.stadtVerbrauch})"
     def __str__(self):
-        return f"{self.__class__.__name__} Attribute:\n {self.efficencyTableName}\n {self.priceListName}\n {self.consumptionListName}\n {self.staedteListe}\n\n {self.verbrauchsListe}\n\n {self.warenNamen}\n\n {self.stadtVerbrauch}\n\n"
+        return f"{self.__class__.__name__} Attribute:\n {self.efficencyTableFile}\n {self.priceListFile}\n {self.consumptionFile}\n {self.staedteListe}\n\n {self.verbrauchsListe}\n\n {self.warenNamen}\n\n {self.stadtVerbrauch}\n\n"
 
     def prepareTables(self):
-        # create city list
-        with open(self.efficencyTableName) as fHandle:
-            line = fHandle.readline()
-            line = line.strip()
-            for nr, name in enumerate(line.split(";")):
-                self.staedteListe[name] = nr        
         # create verbrauchsliste
-        with open(self.consumptionListName) as fHandle:
+        with open(self.consumptionFile) as fHandle:
             for nr, line in enumerate(fHandle):
                 line = line.strip()
                 if nr == 0:
-                    if line == "Ware;Reiche;Wohlhabende;Arme":
+                    if line == "Ware,Reiche,Wohlhabende,Arme":
                         continue
                     else:
                         raise Exception(f"Format of Consumption file not as expected!")
-                currEff = line.split(";")
+                currEff = line.split(",")
                 self.verbrauchsListe.append([])
                 for value in currEff[1:]:
                     self.verbrauchsListe[nr-1].append(float(value))
@@ -101,9 +93,9 @@ class Verbrauchsrechner(object):
         return
 
     def getStadtLineForUpdate(self):
-        line = self.stadtName + ";"
+        line = self.stadtName + ","
         for value in self.stadtVerbrauch:
-            line += str(value) + ";"
+            line += str(value) + ","
         line = line[:-1]
         #line += "\n"
         return line.encode("Utf-8")
@@ -117,8 +109,8 @@ class Verbrauchsrechner(object):
 
         if data:
             for idx, line in enumerate(data):
-                values = line.decode("Utf-8").strip().split(";")
-                #values = line.split(";")
+                values = line.decode("Utf-8").strip().split(",")
+                #values = line.split(",")
                 if (values[0] == self.stadtName):
                     # found city in file, update the line
                     if (len(data) == idx +1):
@@ -144,19 +136,18 @@ class Verbrauchsrechner(object):
         with open(fileName) as fHandle:
             for line in fHandle:
                 line = line.strip()
-                values = line.split(";")
+                values = line.split(",")
                 for idx, value in enumerate(values[1:]):
                     gesamtWarenVerbrauch[idx] += int(value)
         return gesamtWarenVerbrauch
 
     def printAllCities(self, fileName):
+        # workaround for city names
         oldName = self.stadtName
-        with open(fileName) as fHandle:
+        with open(fileName, "rb") as fHandle:
             for line in fHandle:
-                line = line.strip()
-                values = line.split(";")
+                values = line.decode("Utf-8").strip().split(",")
                 if (len(values) > 1):
-                    # workaround for city names
                     self.stadtName = values[0]
                     self.printVerbrauch(values[1:], f"\n Gesamtverbrauch der Stadt: {values[0]} \n")
         self.stadtName = oldName
@@ -215,7 +206,7 @@ class Verbrauchsrechner(object):
         data = []
         efficiency = []
         if self.stadtName != "":
-            with open(self.efficencyTableName, "rb") as fHandle:
+            with open(self.efficencyTableFile, "rb") as fHandle:
                 for line in fHandle:
                     data.append(line.decode("Utf-8").strip().split(","))        
             for line in data:
